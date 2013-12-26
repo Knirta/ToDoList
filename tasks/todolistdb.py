1f0f2f0f0f479da333c63658832808522cc889bf
Knirta <myknirtak@gmail.com>
2013-12-26 04:10:50 +0200

    Added ideas for future code

--- tasks/todolistdb.py
+++ tasks/todolistdb.py
@@ -1,25 +1,41 @@
+#file 1 mydb.py
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="Install_new!", db="todolist")
+class MyDB(object):
+    def init(self):
+      db = MySQLdb.connect(host="localhost", user="root", passwd="Install_new!", db="todolist")
+      cursor = db.cursor()
 
-cursor = db.cursor()
+    def select(self, tuple_tables, kwargs):
+      sql = "select * from users"
+      columns_str = kwargs.values()
+      tab
+      cursor.execute(sql)
+      result = cursor.fetchall()
+      return result
+ 
+   def insert(self, tuple_tables, kwargs_values):
+      sql = "INSERT INTO users(fname, lname, login) VALUE ('Alan', 'Harland', 'aharl')"
 
-sql = "INSERT INTO users(fname, lname, login) VALUE ('Alan', 'Harland', 'aharl')"
+      try:
+        cursor.execute(sql)
+        db.commit()
+      except:
+        print "error"
+        db.rollback()
 
-try:
-    cursor.execute(sql)
-    db.commit()
-except:
-    print "error"
-    db.rollback()
+#file 2 lab1.py
 
-sql = "select * from users"
-cursor.execute(sql)
+from mydb import MyDB
 
-result = cursor.fetchall()
+mydb = MyDB()
+mydb.init()
+
+result = mydb.select('users','fname')
+mydb.insert('users', {'fname' : 'Alan',...}
 
 for rows in result:
     print rows
 
-
 db.close()
+
